97c1622d695d6625a8965c31938bb20ccf1b9767
Fernando Rivas <frivas@silicon-systems.com.ar>
2014-02-25 01:29:33 -0300

    correccion query imagenes

--- src/SSystems/EstocBundle/Entity/UserRepository.php
+++ src/SSystems/EstocBundle/Entity/UserRepository.php
@@ -15,7 +15,7 @@ class UserRepository extends EntityRepository
     public function findUserWithImage($id){
         $query = $this->createQueryBuilder('usr')
             ->select('usr','img','clpf','cnt')
-            ->join('usr.images','img')
+            ->leftJoin('usr.images','img')
             ->leftJoin('img.contract','cnt')
             ->join('usr.collaboratorProfile','clpf')
             ->where('usr.id = :id')
